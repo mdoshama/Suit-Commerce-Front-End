2331b787df137967b315ed8e22777119dd30a9bf
oshama <info.oshama@gmail.com>
2023-10-19 22:18:57 +0530

    components-layoutt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {ContactUsComponent} from "./contact-us/contact-us.component";
+import {AppComponent} from "./app.component";
+import {ProductPageComponent} from "./product-page/product-page.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"''" , component : AppComponent},
+  {path:"home" , component : HomeComponent},
+  {path:"products" , component : ProductPageComponent},
+  {path:"about-us" , component : AboutUsComponent},
+  {path:"contact-us" , component : ContactUsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
